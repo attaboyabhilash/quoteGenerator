644bb770da1de2ec3851fe5361d96616bdb21587
Abhilash Negi <tiger.abhi.3609@gmail.com>
2020-08-25 20:16:20 +0530

    Updates

---
+++ precache-manifest.647d1fa366ebcf8751c9da4f50d361ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0afd6fc457912f2a36fdac02d483654",
+    "url": "/quoteGenerator/index.html"
+  },
+  {
+    "revision": "2c54e4ced6f1479cc912",
+    "url": "/quoteGenerator/static/css/main.8edb52a6.chunk.css"
+  },
+  {
+    "revision": "5da859ffb50cd8a69880",
+    "url": "/quoteGenerator/static/js/2.da54a5bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quoteGenerator/static/js/2.da54a5bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c54e4ced6f1479cc912",
+    "url": "/quoteGenerator/static/js/main.788bcd72.chunk.js"
+  },
+  {
+    "revision": "dec1e930238b4341359f",
+    "url": "/quoteGenerator/static/js/runtime-main.369b6d9c.js"
+  },
+  {
+    "revision": "ab462df0add3841698b1abd7a689116b",
+    "url": "/quoteGenerator/static/media/myMainLogo.ab462df0.svg"
+  }
+]);
